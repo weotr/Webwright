ce87451a6d39fdaeac403f50ff24837c0b7ebac5
qrunpn <qrunpn@outlook.com>
2026-06-16 17:54:03 +0800

    chore: clean pli_skill

--- src/webwright/run/run_command.sh
+++
@@ -1,6 +0,0 @@
-python -m webwright.run.cli \
-    -c base.yaml -c model_openai.yaml \
-    -t "Search for flights from SEA to JFK on 2026-08-15 to 2026-08-20" \
-    --start-url https://www.google.com/flights \
-    --task-id demo_openai \
-    -o outputs/default
